3d90bd63b38186be4ef82e2ca7937182633515a0
Nicole Santamaria <nicolersantamaria@gmail.com>
2017-05-12 12:52:17 -0700

    add get and post on app.rb to include brands for stores.

--- app.rb
+++ app.rb
@@ -10,3 +10,22 @@ get("/") do
   @stores = Store.all()
   erb(:index)
 end
+
+post("/stores") do
+  name = params.fetch("new_store")
+  store = Store.create({:name => name})
+  redirect("/")
+end
+
+get("/stores/:id") do
+  @store = Store.find(params.fetch("id").to_i)
+  erb(:store)
+end
+
+post("/stores/:id/brands") do
+  store_id = params.fetch("id").to_i
+  name = params.fetch("new_brand")
+  price = params.fetch("new_brand_price")
+  brand = Brand.create({:name => name, :price => price, :store_ids => [store_id]})
+  redirect("/stores/#{store_id}")
+end
