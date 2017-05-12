7530442e3ed7a811e7e25c9f7b039953a764c0b3
Nicole Santamaria <nicolersantamaria@gmail.com>
2017-05-12 16:18:49 -0700

    add spec to add brands to store. pass spec.

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -26,7 +26,22 @@ describe('the add brand path', {:type => :feature}) do
   it('has a field that will allow users to create a new brand to add to the brand list') do
     visit('/')
     fill_in('new_brand', :with => 'nike')
+    fill_in('new_brand_price', :with => '20')
     click_button('Add Brand')
     expect(page).to have_content('Nike')
+  end
+end
+
+describe('the add brand to store path', {:type => :feature}) do
+  it('will add brands to a store') do
+    visit('/')
+    fill_in('new_store', :with => 'nordstrom')
+    click_button('Add Store')
+    fill_in('new_brand', :with => 'nike')
+    fill_in('new_brand_price', :with => '20')
+    click_button('Add Brand')
+    click_link('Nordstrom')
+    click_button('Add Brand')
+    expect(page).to have_content('Click on brand names to learn more:')
   end
 end
