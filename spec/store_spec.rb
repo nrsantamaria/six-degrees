e63fb3c383239a0e1547bdad1dd5a329d0ac3db4
Nicole Santamaria <nicolersantamaria@gmail.com>
2017-05-12 11:13:10 -0700

    add a callback for Store class to confirm that name of store has first letter capitalized after saving. pass spec.

--- spec/store_spec.rb
+++ spec/store_spec.rb
@@ -6,4 +6,9 @@ describe(Store) do
     brand2 = Brand.create({:name => "adidas", :price => "40.00", :store_ids => [store1.id]})
     expect(store1.brands()).to(eq([brand1, brand2]))
   end
+  it("will upcase the first letter of the name") do
+    store1 = Brand.new({:name => "macys"})
+    store1.save
+    expect(store1.name).to(eq("Macys"))
+  end
 end
