40423f927208aab9b4ca2a280af5741876561d64
Nick Powell and Nicole Santamaria <nick@>
2017-05-18 13:29:17 -0700

    update gifs. update text on erb pages.

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,4 @@
+
 var myIndex = 0;
 function carousel() {
   var i;
@@ -8,9 +9,36 @@ function carousel() {
   myIndex++;
   if (myIndex > x.length) {myIndex = 1}
   x[myIndex-1].style.display = "block";
-  setTimeout(carousel, 2000); // Change image every 2 seconds
+  setTimeout(carousel, 4000); // Change image every 4 seconds
 }
 
+var myIndex2 = 0;
+function carousel2() {
+  var i;
+  var x = document.getElementsByClassName("mySlides2");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  myIndex2++;
+  if (myIndex2 > x.length) {myIndex2 = 1}
+  x[myIndex2-1].style.display = "block";
+  setTimeout(carousel2, 3500); // Change image every 3 seconds
+}
+
+var myIndex3 = 0;
+function carousel3() {
+  var i;
+  var x = document.getElementsByClassName("mySlides3");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  myIndex3++;
+  if (myIndex3 > x.length) {myIndex3 = 1}
+  x[myIndex3-1].style.display = "block";
+  setTimeout(carousel3, 5000); // Change image every 6 seconds
+}
 $(document).ready(function(){
   carousel();
+  carousel2();
+  carousel3();
 });
