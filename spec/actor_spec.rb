f11737b4419ae93c8effd4dfaee2c107434de406
Nicole Santamaria <nicolersantamaria@gmail.com>
2017-08-27 17:36:49 -0700

    update actor third spec

--- spec/actor_spec.rb
+++ spec/actor_spec.rb
@@ -26,6 +26,6 @@ describe(Actor) do
     movie2 = Movie.create({:title => "Don Air", :year => "1999", :actor_ids => [actor1.id]})
     movie3 = Movie.create({:title => "The Cobbler", :year => "1999", :actor_ids => [actor2.id, actor3.id]})
     movie4 = Movie.create({:title => "50 First Dates", :year => "2004", :actor_ids => [actor3.id, actor4.id]})
-    expect(actor1.degrees(actor4.id)).to(eq([actor1.name, movie1.title, actor2.name,  movie3.title, actor3.name, movie4.title, actor4.name]))
+    expect(actor1.degrees(actor4.id)).to(eq("#{actor1.name}" "<br><span class='sm_text'> was in </span><br>" "\"#{movie1.title}\"" "<br><span class='sm_text'> with </span><br>" "#{actor2.name}" "<br><span class='sm_text'> who was in </span><br>" "\"#{movie3.title}\"" "<br><span class='sm_text'> with </span><br>" "#{actor3.name}" "<br><span class='sm_text'> who was in </span><br>" "\"#{movie4.title}\"" "<br><span class='sm_text'> with </span><br>" "#{actor4.name}" ))
   end
 end
